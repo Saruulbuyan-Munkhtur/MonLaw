11117cc1fca34d60de4831ac347cc8002c5d742c
Ugtakh <ugtakhbayars@gmail.com>
2020-11-18 12:10:52 +0800

    clear
    app.js deerees cookie ustgav

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,4 @@
-const themeCookieName = "theme";
-const themeDark = "dark";
-const themeLight = "light";
 const body = document.getElementsByTagName("body")[0];
-
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 100));
-  var expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == "") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
 
 function switchTheme() {
   var ico = document.getElementsByClassName("switchIcon");
