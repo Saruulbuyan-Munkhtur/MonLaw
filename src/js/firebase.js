ea60ab64824492d01dd7952dac21e668f09541a4
dashka3003 <73590941+dashka3003@users.noreply.github.com>
2020-11-26 21:51:01 +0800

    doing upload

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,4 +1,4 @@
-var Config = {
+const firebaseConfig = {
     apiKey: "AIzaSyDkvXH4PuJsSfY23aEqb2yjQw6LnOvTBJg",
     authDomain: "monlaw-3dae3.firebaseapp.com",
     databaseURL: "https://monlaw-3dae3.firebaseio.com",
@@ -7,6 +7,6 @@ var Config = {
     messagingSenderId: "365309435778",
     appId: "1:365309435778:web:eef7acb3630652ca2af99b",
     measurementId: "G-9NYYG32YRB"
-};
+  };
 // Initialize Firebase
-firebase.initializeApp(Config);
+firebase.initializeApp(firebaseConfig);
